64d69f40276fae09362f186e25d9822db0c77352
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-04-24 10:40:44 -0400

    salvando

--- curso-laravel/routes/web.php
+++ curso-laravel/routes/web.php
@@ -2,33 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/empresa', function (){
-    return view('site/empresa');
+
+Route::get('admin/dashboard'. function(){
+    return "dashboard";
 });
 
-Route::any('/any', function(){
-    return "A any() permite todo tipo de acesso HTTP (put, delete, get, post)"; //não é recomendado
+Route::get('admin/users'. function(){
+    return "users";
 });
 
-Route::match(['get', 'post'], '/match', function(){
-    return "A match() permite apenas acessos definidos";
-});
-
-Route::get('/produto/{id}/{cat?}', function($id, $cat = ''){
-    return "O id do produto é: " . $id. "<br>" . "A categoria é: " . $cat;
+Route::get('admin/client'. function(){
+    return "client";
 });
