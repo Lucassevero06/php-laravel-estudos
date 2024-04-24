1f60d2f39015e42b576a6fc3c54253390ef4200c
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-04-24 13:49:09 -0400

    Update web.php

--- curso-laravel/routes/web.php
+++ curso-laravel/routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('produtos', ProdutoController::class);
+Route::resource('produtos', ProdutoController::class); 
