efb5e2dc1f26ca3829e09aabe359dd9e428927b6
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-04-24 10:22:09 -0400

    entendendo mais sobre routes e views

--- curso-laravel/routes/web.php
+++ curso-laravel/routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/empresa', function (){
+    return view('site/empresa');
+});
+
+Route::any('/any', function(){
+    return "A any() permite todo tipo de acesso HTTP (put, delete, get, post)"; //não é recomendado
+});
+
+Route::match(['get', 'post'], '/match', function(){
+    return "A match() permite apenas acessos definidos";
+});
+
+Route::get('/produto/{id}/{cat?}', function($id, $cat = ''){
+    return "O id do produto é: " . $id. "<br>" . "A categoria é: " . $cat;
+});
