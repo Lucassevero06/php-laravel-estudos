a1a09af1ce2c85c203fc3de3654e31f9365fdcea
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-04-29 11:13:47 -0400

    tamo evoluindo mlk

---
+++ laravel-curso/resources/views/jogos/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Listagem')
+
+@section('content')
+    <!-- TUDO QUE FOR INSERIDO AQUI DENTRO SERÁ RENDERIZADO NO TEMPLATE PADRÃO (layouts/app) -->
+    <h1>TESTE</h1>
+
+@endsection('content')
