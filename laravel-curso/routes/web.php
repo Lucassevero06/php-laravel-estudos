f60f94c72a590ebbfc530c90c1f07d18cc9e1666
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-04-29 09:44:10 -0400

    routes

--- laravel-curso/routes/web.php
+++ laravel-curso/routes/web.php
@@ -13,6 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+//Route::get('/home', function () {
+//    return view('welcome');
+//});
+
+//Route::view('/jogos', 'jogos');
+
+//Route::view('/jogos', 'jogos',['name'=>'GTA']);
+
+//Route::get('/jogos/{id?}/{name?}', function($id=null, $name=null){
+//    return view('jogos', ['idJogo'=>$id, 'nameJogo'=>$name]);
+//})->where('id', '[0-9]+', 'name', '[a-z]+');
+
+Route::get('/jogos', function () {
+    return view('jogos');
+});
+
+Route::get('/home', function () {
     return view('welcome');
+})->name('home');
+
+//o fallback() faz com que quando a rota que é chamada não foi encontrada ou não existe, ele envia para o fallback(), que é uma tela totalmente customizavel.
+Route::fallback(function(){
+    return "ROTA NÃO ENCONTRADA!";
 });
